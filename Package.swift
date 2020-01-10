cce834d03c83b6ee346c49bdda56295793f91a5e
Gerald Brigen <gbrigens@gmail.com>
2020-01-10 18:10:05 +0300

    Update SMP
    Update SMP to support iOS 13.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,11 @@
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+  name: "Locksmith",
+  platforms: [
+        .iOS(.v12),
+    ]
+  products: [.library(name: "Locksmith",targets: ["Locksmith"])],
+  targets: [.target(name: "Locksmith",path: "Source")],
+  swiftLanguageVersions: [.v4]
 )
